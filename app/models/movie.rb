09ba524be1ce876037fbe28d3de784d2d185a0a5
Sameer Kumar <vbetan3@tamu.edu>
2020-09-11 19:22:06 +0000

    part 1

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,6 @@
 class Movie < ActiveRecord::Base
+    def self.ratings
+        Movie.select(:rating).distinct.inject([]) { |a, m| a.push m.rating}
+        #['G','R','PG-13','PG'].map{|a| a}#     
+    end    
 end
